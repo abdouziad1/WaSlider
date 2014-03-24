cc223de6a26cc4ceef8dff4503f78dea1077966a
abdouziad1 <abdouziad1@gmail.com>
2014-03-24 10:57:52 +0000

    Modification

--- widget.js
+++ widget.js
@@ -20,13 +20,36 @@ WAF.define('WaSlider', ['waf-core/widget'], function(widget) {
         }),
         step: widget.property({
             type: 'number',
-            defaultValue: 10,
+            defaultValue: 20,
             //bindable: false
         }),
 
         init: function() {
-			this.node.setAttribute("type","range");  
+        	this.render();
+        	var $node = $(this.node);
+        	$node.val(this.value());
+
+        	
+        	this.value.onChange(function(){
+				  $node.val(this.value());
+				  $node.val(this.min());
+				  $node.val(this.max());
+				  $node.val(this.step());
+				  
+				  this.render();
+        	});
+
+        },
+        
+        render: function(){
+        	
+        	this.node.setAttribute("type","range");
+			this.node.setAttribute("min",this.min());
+			this.node.setAttribute("max",this.max());
+			this.node.setAttribute("step",this.step());
+			this.node.setAttribute("value",this.value());
         }
+        
     });
 
 
