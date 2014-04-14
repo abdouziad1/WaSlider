a6e1943c628cf6d1071bf0bca87601bcba2b50a1
abdouziad1 <abdouziad1@gmail.com>
2014-04-14 17:33:56 +0100

    Update Widget.js

--- widget.js
+++ widget.js
@@ -1,56 +1,58 @@
 WAF.define('WaSlider', ['waf-core/widget'], function(widget) {
 
     var SliderWidget = widget.create('WaSlider', {
-    	tagName: 'input',
+        tagName: 'input',
 
-  		/* Properties */
+        /* Properties */
         value: widget.property({
             type: 'number',
-            defaultValue: 20
+            defaultValue: 0
         }),
         min: widget.property({
             type: 'number',
             defaultValue: 0
-            //bindable: false
         }),
         max: widget.property({
             type: 'number',
             defaultValue: 100
-            //bindable: false
         }),
         step: widget.property({
             type: 'number',
-            defaultValue: 20,
-            //bindable: false
+            defaultValue: 1,
         }),
 
         init: function() {
-        	
-        	this.render();
-        	var $node = $(this.node); // jQuery
-				  
-        	this.value.onChange(function(){
-				  $node.val(this.value());	  
-        	});
-        	this.min.onChange(function(){
-        		  $node.attr('min', this.min()); 
-        	});
-        	this.max.onChange(function(){
-        		  $node.attr('max', this.max()); 
-        	});
-        	this.step.onChange(function(){
-        		  $node.attr('step', this.step()); 
-        	});
+            
+            this.render();
+            var $node = $(this.node);
+            
+            $node.on('change', function() {
+                var value = $node.val();
+                this.value(value);
+            }.bind(this));
+                  
+            this.value.onChange(function(){
+                  $node.val(this.value());    
+            });
+            this.min.onChange(function(){
+                  $node.attr('min', this.min()); 
+            });
+            this.max.onChange(function(){
+                  $node.attr('max', this.max()); 
+            });
+            this.step.onChange(function(){
+                  $node.attr('step', this.step()); 
+            });
         },
         
         render: function(){
-        	var $node = $(this.node);
-        	this.node.type = 'range'; //this.node.setAttribute("type","range");
-        	
-        	$node.val(this.value());
-			$node.attr("min",this.min());
-			$node.attr("max",this.max());
-			$node.attr("step",this.step());
+            var $node = $(this.node);
+            this.node.type = 'range';
+            
+            $node.val(this.value());
+            $node.attr("min",this.min());
+            $node.attr("max",this.max());
+            $node.attr("step",this.step());
         }
         
     });
